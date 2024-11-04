ce6b0c9f11289ac08cef2234ac966406b7fb741a
Bread92 <vaneck.van2019@gmail.com>
2024-11-04 06:31:57 +0300

    Add basic message get and create

---
+++ BreadChat/BreadChat.Application/Dtos/MessageDtos/MessageDto.cs
@@ -0,0 +1,18 @@
+﻿using BreadChat.Domain.Entities;
+
+namespace BreadChat.Application.Dtos.MessageDtos;
+
+public class MessageDto
+{
+    public Guid Id { get; set; }
+    public string Text { get; set; }
+    
+    public static MessageDto FromDomain(Message message)
+    {
+        return new MessageDto()
+        {
+            Id = message.Id,
+            Text = message.Text,
+        };
+    }
+}
--- BreadChat/BreadChat.Persistence/Migrations/20241104032541_AddMessageEntity.Designer.cs
+++ BreadChat/BreadChat.Persistence/Migrations/20241104032541_AddMessageEntity.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using BreadChat.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BreadChat.Persistence.Migrations
 {
     [DbContext(typeof(BreadChatDbContext))]
-    partial class BreadChatDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241104032541_AddMessageEntity")]
+    partial class AddMessageEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.10");
